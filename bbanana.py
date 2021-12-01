ee4738b93fee36d37678c130b052e419e2757de0
mjmode1 <46835597+mjmode1@users.noreply.github.com>
2021-12-01 11:44:09 +0900

    Update bbanana.py

--- bbanana.py
+++ bbanana.py
@@ -18,5 +18,5 @@ async def on_message(message):
         await message.channel.send("반가워!")
         
       
-access_token = os.getenv("BOT_TOKEN")
+access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
